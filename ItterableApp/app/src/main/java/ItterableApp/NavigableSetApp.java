1b24daa3abec066d51aa8ea3cecdda941c353dfd
HaiDiazo <terubozu119@gmail.com>
2022-05-21 11:43:16 +0700

    java_collection

--- ItterableApp/app/src/main/java/ItterableApp/NavigableSetApp.java
+++ ItterableApp/app/src/main/java/ItterableApp/NavigableSetApp.java
@@ -4,10 +4,33 @@
  */
 package ItterableApp;
 
+import java.util.NavigableSet;
+import java.util.Set;
+import java.util.TreeSet;
+
 /**
  *
  * @author Mr Nathan
  */
 public class NavigableSetApp {
-    
+    public static void main(String[] args)
+    {
+//     Urutin secara Ascending
+        NavigableSet<String> names = new TreeSet<>();
+        names.addAll(Set.of("Nathan", "Aptanta", "Parama", "Programmer"));
+        
+//      Urutin secara Descending
+        NavigableSet<String> namesDesc = names.descendingSet();
+        
+//      Dikasih untuk slicing dari depan ke belakang
+        NavigableSet<String> aptanta = names.headSet("Parama", true);
+        
+//      Untuk slicing dari belakang ke depan
+        NavigableSet<String> parama = names.tailSet("Parama", true);
+        
+        
+        for (String name : parama) {
+            System.out.println(name);
+        }
+    }
 }
