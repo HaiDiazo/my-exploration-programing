a35913f41925cd7288221fe5256b88c328750cf3
HaiDiazo <terubozu119@gmail.com>
2022-05-25 22:18:09 +0700

    apache maven

---
+++ belajar-apache-maven/belajar-apache-maven-app/src/main/java/coba/apache/maven/maven/App.java
@@ -0,0 +1,21 @@
+package coba.apache.maven.maven;
+
+import com.google.gson.Gson;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Gson gson = new Gson();
+
+        Person person = new Person("Nathaniel");
+        String json = gson.toJson(person);
+
+        System.out.println(json);
+//        System.out.println( "Hello World!" );
+    }
+}
---
+++ belajar-apache-maven/belajar-apache-maven-data/src/main/java/coba/apache/maven/maven/Person.java
@@ -0,0 +1,18 @@
+package coba.apache.maven.maven;
+
+public class Person {
+
+    private String name;
+
+    public Person(String name){
+        this.name = name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
